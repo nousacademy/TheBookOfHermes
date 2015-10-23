0d465f8dc8e5f4b5e2db6baf22ba92fe9e6b8e2b
Labs-Moov <alessandro.elkan@borderfree.com>
2015-10-23 10:24:27 -0400

    s

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-
- // app.use(express.static(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, '/hermeticSrc')));
 
 
@@ -21,11 +19,6 @@ var server = app.listen(3000, function() {
 app.post('/post', function(req, res){
 
 	res.send('got it')
-})
+});
 
-//unused stuff
-
-// app.post("/ask", function(req, res) {
-
-// });
 
