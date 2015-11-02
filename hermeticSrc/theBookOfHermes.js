b1d92242b727548f7f87f901db92c8fb0eec50f9
Labs-Moov <alessandro.elkan@borderfree.com>
2015-11-02 12:00:20 -0500

    edit

--- hermeticSrc/theBookOfHermes.js
+++ hermeticSrc/theBookOfHermes.js
@@ -161,32 +161,3 @@
         window.Hermes = theAll();
     }
 })(window);
-
-var Car = (function() {
-
-  // Create a store to hold the private objects.
-  var privateStore = {};
-  var uid = 0;
-
-  function Car(mileage) {
-    // Create an object to manage this instance's state and
-    // use a unique ID to reference it in the private store.
-    privateStore[this.id = uid++] = {};
-    // Store private stuff in the private store
-    // instead of on `this`.
-    privateStore[this.id].mileage = mileage || 0;
-  }
-
-  Car.prototype.drive = function(miles) {
-    if (typeof miles == 'number' && miles > 0)
-      privateStore[this.id].mileage += miles;
-    else
-      throw new Error('drive can only accept positive numbers');
-  };
-
-  Car.prototype.readMileage = function() {
-    return privateStore[this.id].mileage;
-  };
-
-  return Car;
-}());
